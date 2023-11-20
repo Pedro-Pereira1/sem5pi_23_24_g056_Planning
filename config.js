6862476adcf55755c2d2282a6833723f33d1e483
JoseGouveia <1211089@isep.ipp.pt>
2023-11-20 12:23:56 +0000

    Added initial structure to planning module

---
+++ config.js
@@ -0,0 +1,48 @@
+import dotenv from 'dotenv';
+
+// Set the NODE_ENV to 'development' by default
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+const envFound = dotenv.config();
+if (!envFound) {
+  // This error should crash whole process
+
+  throw new Error("⚠️  Couldn't find .env file  ⚠️");
+}
+
+export default {
+  /**
+   * Your favorite port : optional change to 4000 by JRT
+   */
+  port: parseInt(process.env.PORT, 10) || 4000,
+
+  /**
+   * That long string from mlab
+   */
+  databaseURL: process.env.MONGODB_URI || "mongodb://mongoadmin:4a5627f241f602c27f1b4f7d@vsgate-s1.dei.isep.ipp.pt:11098/?authMechanism=DEFAULT",
+
+  /**
+   * Your secret sauce
+   */
+  jwtSecret: process.env.JWT_SECRET || "my sakdfho2390asjod$%jl)!sdjas0i secret",
+
+  /**
+   * Used by winston logger
+   */
+  logs: {
+    level: process.env.LOG_LEVEL || 'info',
+  },
+
+  /**
+   * API configs
+   */
+  api: {
+    prefix: '/api',
+  },
+
+  controllers: {},
+
+  repos: {},
+
+  services: {}
+};
