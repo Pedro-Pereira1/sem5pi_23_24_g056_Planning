c2d85fcdc57bb1f6ebed8408cf20a9716de6b860
Pedro Pereira <1211131@isep.ipp.pt>
2023-11-26 14:53:05 +0000

    Refactor floor sequence extraction in Algorithms.pl

--- Algorithms.pl
+++ Algorithms.pl
@@ -135,69 +135,69 @@ corredor(b,c,b3,c3).
 %
 %
 %
-%m(col,lin,valor)
-m(1,1,1).
-m(2,1,1).
-m(3,1,1).
-m(4,1,1).
-m(5,1,1).
-m(6,1,1).
-m(7,1,1).
-m(8,1,1).
+%m(col,lin,valor,Piso)
+m(1,1,1,a1).
+m(2,1,1,a1).
+m(3,1,1,a1).
+m(4,1,1,a1).
+m(5,1,1,a1).
+m(6,1,1,a1).
+m(7,1,1,a1).
+m(8,1,1,a1).
 
-m(1,2,0).
-m(2,2,0).
-m(3,2,0).
-m(4,2,0).
-m(5,2,0).
-m(6,2,0).
-m(7,2,0).
-m(8,2,1).
+m(1,2,0,a1).
+m(2,2,0,a1).
+m(3,2,0,a1).
+m(4,2,0,a1).
+m(5,2,0,a1).
+m(6,2,0,a1).
+m(7,2,0,a1).
+m(8,2,1,a1).
 
-m(1,3,0).
-m(2,3,0).
-m(3,3,0).
-m(4,3,0).
-m(5,3,0).
-m(6,3,0).
-m(7,3,0).
-m(8,3,1).
+m(1,3,0,a1).
+m(2,3,0,a1).
+m(3,3,0,a1).
+m(4,3,0,a1).
+m(5,3,0,a1).
+m(6,3,0,a1).
+m(7,3,0,a1).
+m(8,3,1,a1).
 
-m(1,4,0).
-m(2,4,0).
-m(3,4,0).
-m(4,4,0).
-m(5,4,0).
-m(6,4,0).
-m(7,4,0).
-m(8,4,1).
+m(1,4,0,a1).
+m(2,4,0,a1).
+m(3,4,0,a1).
+m(4,4,0,a1).
+m(5,4,0,a1).
+m(6,4,0,a1).
+m(7,4,0,a1).
+m(8,4,1,a1).
 
-m(1,5,1).
-m(2,5,1).
-m(3,5,1).
-m(4,5,1).
-m(5,5,0).
-m(6,5,0).
-m(7,5,0).
-m(8,5,1).
+m(1,5,1,a1).
+m(2,5,1,a1).
+m(3,5,1,a1).
+m(4,5,1,a1).
+m(5,5,0,a1).
+m(6,5,0,a1).
+m(7,5,0,a1).
+m(8,5,1,a1).
 
-m(1,6,1).
-m(2,6,1).
-m(3,6,1).
-m(4,6,1).
-m(5,6,0).
-m(6,6,0).
-m(7,6,0).
-m(8,6,1).
+m(1,6,1,a1).
+m(2,6,1,a1).
+m(3,6,1,a1).
+m(4,6,1,a1).
+m(5,6,0,a1).
+m(6,6,0,a1).
+m(7,6,0,a1).
+m(8,6,1,a1).
 
-m(1,7,1).
-m(2,7,1).
-m(3,7,1).
-m(4,7,1).
-m(5,7,0).
-m(6,7,0).
-m(7,7,0).
-m(8,7,1).
+m(1,7,1,a1).
+m(2,7,1,a1).
+m(3,7,1,a1).
+m(4,7,1,a1).
+m(5,7,0,a1).
+m(6,7,0,a1).
+m(7,7,0,a1).
+m(8,7,1,a1).
 
 
 %===========================================================================================================
@@ -252,7 +252,8 @@ segue_pisos(PisoAct,PisoDest,[EdAct,EdSeg|LOutrosEd],[elev(PisoAct,PisoAct1),cor
 
 melhor_caminho_pisos(PisoOr,PisoDest,LLigMelhor,LPsCam):-
 	findall(LLig,caminho_pisos(PisoOr,PisoDest,_,LLig,LPsCam),LLLig),
-	menos_elevadores(LLLig,LLigMelhor,_,_).
+	menos_elevadores(LLLig,LLigMelhor,_,_),
+	extract_floor_sequence(LLigMelhor,LPsCam).
 
 menos_elevadores([LLig],LLig,NElev,NCor):-
 	conta(LLig,NElev,NCor).
@@ -270,11 +271,22 @@ conta([elev(_,_)|L],NElev,NCor):- conta(L,NElevL,NCor),NElev is NElevL+1.
 conta([cor(_,_)|L],NElev,NCor):-conta(L,NElev,NCorL),NCor is NCorL+1.
 
 %===========================================================================================================
+% 
+
+extract_floor_sequence([], []).
+extract_floor_sequence([elev(From, To) | Rest], [From, To | Result]) :-
+    extract_floor_sequence(Rest, Result).
+extract_floor_sequence([cor(_, To) | Rest], [To | Result]) :-
+    extract_floor_sequence(Rest, Result).
+
+%===========================================================================================================
 % Cria o grafo de ligacoes entre celulas (celulas com 0 estão ligadas a celulas com 0) == cria_grafo(8,7).
 
 cria_grafo(_,0):-!.
-cria_grafo(Col,Lin):-cria_grafo_lin(Col,Lin),Lin1 is Lin-1,cria_grafo(Col,Lin1).
-
+cria_grafo(Col,Lin):-
+	cria_grafo_lin(Col,Lin),
+	Lin1 is Lin-1,
+	cria_grafo(Col,Lin1).
 
 cria_grafo_lin(0,_):-!.
 cria_grafo_lin(Col,Lin):-m(Col,Lin,0),!,ColS is Col+1, ColA is Col-1, LinS is Lin+1,LinA is Lin-1,
