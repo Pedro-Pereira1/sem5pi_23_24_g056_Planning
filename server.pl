aed9ac9d83b642c9c1a67b19df54679e21b45ae3
Tiago <1211128@isep.ipp.pt>
2024-01-02 19:12:15 +0000

    Update user authentication logic

--- server.pl
+++ server.pl
@@ -45,9 +45,9 @@ get_path(Request):-
         ]),
     atom_string(Origem, Or),
     atom_string(Destino, Dest),
-    robot_path(Or, Dest, LLigMelhor, LPisMelhor, CamF),
+    robot_path(Or, Dest, LLigMelhor, LPisMelhor, CamF, CustoFinal),
 
-    cells_lists_to_json_object(CamF, LPisMelhor, LLigMelhor, JsonObject),
+    cells_lists_to_json_object(CamF, LPisMelhor, LLigMelhor, CustoFinal, JsonObject),
 
     reply_json(JsonObject, [json_object(dict)]),
     !.
@@ -85,15 +85,15 @@ lists_to_lists([H|T], [LH|LT]) :-
     list_to_list(H, LH),
     lists_to_lists(T, LT).
 
-cells_lists_to_json_object([], _, _, JsonObject):- 
-    JsonObject = json([cells=[], floorIds=[], accPoints=[]]).
-cells_lists_to_json_object(CellsLists, [], [], JsonObject):- 
+cells_lists_to_json_object([], _, _, _, JsonObject):- 
+    JsonObject = json([cells=[], floorIds=[], accPoints=[], custo=0]).
+cells_lists_to_json_object(CellsLists, [], [], Custo, JsonObject):- 
     lists_to_lists(CellsLists, List),
-    JsonObject = json([cells=List, floorIds=[], accPoints=[]]).
+    JsonObject = json([cells=List, floorIds=[], accPoints=[], custo=Custo]).
 cells_lists_to_json_object(CellsLists, FloorIds, ElevsAndCors, JsonObject) :-
     lists_to_lists(CellsLists, List),
     convert_elevs_and_cors(ElevsAndCors, ElevsAndCorsList),
-    JsonObject = json([cells=List, floorIds=FloorIds, accPoints=ElevsAndCorsList]).
+    JsonObject = json([cells=List, floorIds=FloorIds, accPoints=ElevsAndCorsList, custo = Custo]).
 
 convert_elevs_and_cors([], []).
 convert_elevs_and_cors([elev(X,Y)|T], [[X,elev,Y]|ET]) :-
@@ -490,6 +490,8 @@ delete_robotType():-
 %===========================================================================================================
 % Encontrar um caminho entre dois edificios
 
+% Encontrar um caminho entre dois edificios
+
 caminho_edificios(Start,End,Path):-
 	caminho_edificios(Start,End,[Start],Path).
 	
@@ -646,9 +648,9 @@ is_elevador(Point) :-
     ElevadorType = elev(_, _).
 
 
-caminho_completo(Point, Point, [], [Points]).
+caminho_completo(Point, Point, [], [Points], Custo).
 
-caminho_completo(Point, Point2, [],[Path1]):-
+caminho_completo(Point, Point2, [],[Path1], Custo):-
 	ponto_acesso(Point,PisoOr,Orig),
 	ponto_acesso(Point2,PisoDest,Dest),
 	 (PisoOr = PisoDest ->
@@ -663,7 +665,6 @@ caminho_completo(Point, Point2, [],[Path1]):-
 			%write('Same floor!'), nl,
 			%write('==========================='), nl,
             aStar(Orig, Dest, Path1, Custo, PisoOr)
-            %write('Path1: '), write(Path1), nl
 			%Path1 = [a], Custo = 0,
             %caminho_completo(Point2, Point2, [], Path1)
         ;
@@ -689,11 +690,12 @@ caminho_completo(Point, Point2, [],[Path1]):-
         ).
 
 
-caminho_completo(Point1, Point2, [PointX|T], [Path1|RestPath]) :-
+caminho_completo(Point1, Point2, [PointX|T], [Path1|RestPath], Custo) :-
 	ponto_acesso(Point1,PisoOr,Orig),
 	ponto_acesso(PointX,PisoDest,Dest),
 	(is_elevador(Point1), is_elevador(PointX) ->
-		caminho_completo(PointX, Point2, T, RestPath)	
+		caminho_completo(PointX, Point2, T, RestPath,SubCost),
+		Custo is SubCost	
     ;
         (PisoOr = PisoDest ->
 			%write('==========================='), nl,
@@ -707,8 +709,8 @@ caminho_completo(Point1, Point2, [PointX|T], [Path1|RestPath]) :-
 			%write('==========================='), nl,
             aStar(Orig, Dest, Path1, Custo, PisoOr),
 			%Path1 = [a], Custo = 0,
-            %write('Path1: '), write(Path1), nl,
-            caminho_completo(PointX, Point2, T, RestPath)
+            caminho_completo(PointX, Point2, T, RestPath, RemainingCost),
+			 Custo is SubCost + RemainingCost
         ;
 			(ponto_acesso(Point1,PisoDest,New)->
 			%write('==========================='), nl,
@@ -721,25 +723,24 @@ caminho_completo(Point1, Point2, [PointX|T], [Path1|RestPath]) :-
 			%write('Same floor!'), nl,
 			%write('==========================='), nl,
 			%Path1 = [b], Custo = 1,
-			aStar(New, Dest, Path1, Custo, PisoDest),
-            %write('Path1: '), write(Path1), nl,
-            caminho_completo(PointX, Point2, T, RestPath)
+			aStar(New, Dest, Path1, SubCost, PisoDest),
+            caminho_completo(PointX, Point2, T, RestPath, RemainingCost),
+			Custo is SubCost + RemainingCost
 			);
             Path1 = [C], Custo = 1,
-			caminho_completo(PointX, Point2, T, RestPath)
+			caminho_completo(PointX, Point2, T, RestPath, RemainingCost),
+        	Custo is SubCost + RemainingCost
         )
     ).
 	
 %===========================================================================================================
 % RobotPath -> caminho entre dois pontos de acesso
 
-robot_path(P1,P2, LLigMelhor, LPisMelhor, List) :-
-	ponto_acesso(P1,PisoOr,Orig),!,
+robot_path(P1,P2, LLigMelhor, LPisMelhor, List, CAM) :-
+	ponto_acesso(P1,PisoOr,Orig),
 	ponto_acesso(P2,PisoDest,Dest),
 	melhor_caminho_pisos(PisoOr,PisoDest,LLigMelhor, LPisMelhor),
-    write('LLigMelhor: '), write(LLigMelhor), nl,
-    write('LPisMelhor: '), write(LPisMelhor), nl,
-	caminho_completo(P1,P2, LLigMelhor, List).
+	caminho_completo(P1,P2, LLigMelhor, List, CAM).
 
 %===========================================================================================================
 % Predicate to print all edges
@@ -748,20 +749,352 @@ print_all_edges(Graph) :-
     format('Edge: ~w -> ~w (Weight: ~w)~n', [Start, End, Weight]),
     fail. % This predicate will fail intentionally to stop backtracking after printing all edges.
 
-count_nodes():-
-    node(Col, Lin, Val, Floor),
-    format('Node: (~w, ~w, ~w, ~w)~n', [Col, Lin, Val, Floor]),
-    fail. % This predicate will fail intentionally to stop backtracking after printing all edges..
 
-write_nodes(Floor):-
-    findall(node(X, Y, Z, Floor), node(X, Y, Z, Floor), Nodes),
-    write('Length: '), length(Nodes, Length), write(Length), nl.
-    %write(Nodes), nl.
+%===========================================================================================================
+% Algoritmo genetico
+
+:-dynamic geracoes/1.
+:-dynamic populacao/1.
+:-dynamic prob_cruzamento/1.
+:-dynamic prob_mutacao/1.
+:-dynamic estabilizacao_solucao/1.
+:-dynamic melhor_sequencia/1.
+:-dynamic tempo_max/1.
+:-dynamic inicio/1.
+:-dynamic entre_tarefas/3.
+
+
+tarefa(t1,"K1","A201").	% tarefa(Id,TempoProcessamento).
+tarefa(t2,"A207","B201").
+tarefa(t3,"R1","BENG").
+tarefa(t4,"R2","A209").
+tarefa(t5,"K1","K2").
+
+%entre_tarefas(t1,t2,3).	% tarefa(Id, IdTarefa1, IdTarefa2, TempoProcessamento).
+%entre_tarefas(t1,t3,1).
+%entre_tarefas(t1,t4,6).
+%entre_tarefas(t1,t5,4).
+%
+%entre_tarefas(t2,t1,1).
+%entre_tarefas(t2,t3,6).
+%entre_tarefas(t2,t4,9).
+%entre_tarefas(t2,t5,3).
+%
+%entre_tarefas(t3,t1,2).
+%entre_tarefas(t3,t2,5).
+%entre_tarefas(t3,t4,1).
+%entre_tarefas(t3,t5,5).
+%
+%entre_tarefas(t4,t1,3).
+%entre_tarefas(t4,t2,2).
+%entre_tarefas(t4,t3,1).
+%entre_tarefas(t4,t5,7).
+%
+%entre_tarefas(t5,t1,2).
+%entre_tarefas(t5,t2,5).
+%entre_tarefas(t5,t3,1).
+%entre_tarefas(t5,t4,8).
+
+% tarefas(NTarefas).
+tarefas(5).
+
+% parameterizacao
+inicializa:-
+	%write('Numero de novas Geracoes: '),read(NG), 
+	NG is 100,
+	(retract(geracoes(_));true), asserta(geracoes(NG)),
+
+	%write('Dimensao da Populacao: '),read(DP), 
+	DP is 5,
+	(retract(populacao(_));true), asserta(populacao(DP)),
+
+	%write('Probabilidade de Cruzamento (%):'), 
+	%read(P1), PC is P1/100,  
+	PC is 70/100,
+	(retract(prob_cruzamento(_));true),asserta(prob_cruzamento(PC)),
+
+	%write('Probabilidade de Mutacao (%):'), 
+	%read(P2), PM is P2/100, 
+	PM is 5/100,
+	(retract(prob_mutacao(_));true), asserta(prob_mutacao(PM)).
+
+	% Estabilizacao da solucao
+	estabilizacao_solucao(10).
+
+	% Tempo maximo de execucao
+	tempo_max(20).
+
+gera:-
+    (retract(inicio(_));true),
+	inicializa,
+    get_time(V),
+	asserta(inicio(V)),
+	gera_populacao(Pop),
+	write('Pop='),write(Pop),nl,
+	avalia_populacao(Pop,PopAv),
+	write('PopAv='),write(PopAv),nl,
+	ordena_populacao(PopAv,PopOrd),
+	geracoes(NG),
+	PopOrd = [FS|_],
+	gera_geracao(0,NG,PopOrd,[FS,FS,0]).
+
+gera_populacao(Pop):-
+	populacao(TamPop),
+	tarefas(NumT),
+	findall(Tarefa,tarefa(Tarefa,_,_),ListaTarefas),
+	gera_populacao(TamPop,ListaTarefas,NumT,Pop).
+
+gera_populacao(0,_,_,[]):-!.
+
+gera_populacao(TamPop,ListaTarefas,NumT,[Ind|Resto]):-
+	TamPop1 is TamPop-1,
+	gera_populacao(TamPop1,ListaTarefas,NumT,Resto),
+	gera_individuo(ListaTarefas,NumT,Ind),
+	not(member(Ind,Resto)).
+gera_populacao(TamPop,ListaTarefas,NumT,L):-
+	gera_populacao(TamPop,ListaTarefas,NumT,L).
+
+gera_individuo([G],1,[G]):-!.
+
+gera_individuo(ListaTarefas,NumT,[G|Resto]):-
+	NumTemp is NumT + 1, % To use with random
+	random(1,NumTemp,N),
+	retira(N,ListaTarefas,G,NovaLista),
+	NumT1 is NumT-1,
+	gera_individuo(NovaLista,NumT1,Resto).
+
+retira(1,[G|Resto],G,Resto).
+retira(N,[G1|Resto],G,[G1|Resto1]):-
+	N1 is N-1,
+	retira(N1,Resto,G,Resto1).
+
+avalia_populacao([],[]).
+avalia_populacao([Ind|Resto],[Ind*V|Resto1]):-
+	avalia(Ind,V),
+	avalia_populacao(Resto,Resto1).
+
+avalia(Seq,V):-
+	Seq = [T|Resto],
+	avalia(Resto,T,V).
+
+avalia([],_,0).
+avalia([T|Resto],Anterior,Total):-
+	avalia(Resto,T,Total1),
+	entre_tarefas(Anterior, T, Dur),
+	Total is Total1+Dur.
+
+ordena_populacao(PopAv,PopAvOrd):-
+	bsort(PopAv,PopAvOrd).
+
+bsort([X],[X]):-!.
+bsort([X|Xs],Ys):-
+	bsort(Xs,Zs),
+	btroca([X|Zs],Ys).
+
+
+btroca([X],[X]):-!.
+
+btroca([X*VX,Y*VY|L1],[Y*VY|L2]):-
+	VX>VY,!,
+	btroca([X*VX|L1],L2).
+
+btroca([X|L1],[X|L2]):-btroca(L1,L2).
+
+% Numero de geracoes maximo atingido.
+gera_geracao(G,G,Pop,_):-!,
+	write('Geracao '), write(G), write(':'), nl, write(Pop), nl,
+	Pop = [Lista*Tempo|_],
+	(retractall(melhor_sequencia(_,_)),!;true),
+	asserta(melhor_sequencia(Lista, Tempo)),
+	write('Numero maximo de geracoes atingido!').
+
+% Numero de estabilizacao maximo atingido.
+gera_geracao(N,_,Pop,[_,_,X]):-
+	estabilizacao_solucao(X),
+	!,
+	write('Geracao '), write(N), write(':'), nl, write(Pop), nl,
+	Pop = [Lista*Tempo|_],
+	(retractall(melhor_sequencia(_,_)),!;true),
+	asserta(melhor_sequencia(Lista, Tempo)),
+	write('Estabilizacao maxima atingida!').
+
+gera_geracao(N,G,Pop,[SolAct,SolAct,X]):-
+	X2 is X+1,
+	gera_geracao2(N,G,Pop,[SolAct,SolAct,X2]).
+
+gera_geracao(N,G,Pop,[SolAnt,SolAct,_]):-
+	gera_geracao2(N,G,Pop,[SolAnt,SolAct,1]).
+
+gera_geracao(N,_,Pop,_):-
+	get_time(Tempo),
+	verifica_tempo(Tempo),
+	!,
+	%write('Geracao '), write(N), write(':'), nl, write(Pop), nl,
+	Pop = [Lista*Tempo|_],
+	(retractall(melhor_sequencia(_,_)),!;true),
+	asserta(melhor_sequencia(Lista, Tempo)),
+	write('Tempo maximo atingido!').
+
+gera_geracao2(N, G, Pop, [_, SolAct, X]):-
+	write('Geracao '), write(N), write(':'), nl, write(Pop), nl,
+	cruzamento(Pop,NPop1,1),
+	NPop1 = [NSol|_],
+	mutacao(NPop1,NPop,1),
+	avalia_populacao(NPop,NPopAv),
+	ordena_populacao(NPopAv,NPopOrd),
+	N1 is N+1,
+	gera_geracao(N1,G,NPopOrd,[SolAct, NSol, X]).
+
+verifica_tempo(TempoAct):-
+	inicio(Ini),
+	tempo_max(Max),
+	TempoAct - Ini >= Max.
+
+gerar_pontos_cruzamento(P1,P2):-
+	gerar_pontos_cruzamento1(P1,P2).
+
+gerar_pontos_cruzamento1(P1,P2):-
+	tarefas(N),
+	NTemp is N+1,
+	random(1,NTemp,P11),
+	random(1,NTemp,P21),
+	P11\==P21,!,
+	((P11<P21,!,P1=P11,P2=P21);(P1=P21,P2=P11)).
+gerar_pontos_cruzamento1(P1,P2):-
+	gerar_pontos_cruzamento1(P1,P2).
+
+cruzamento([],[],_).
+
+%cruzamento([Ind*_],[Ind],_).
+cruzamento([Ind1*_|Resto], [Ind1|Resto1], 1):-
+	!,
+	random_permutation(Resto, RestoBaralhado),
+	cruzamento(RestoBaralhado, Resto1, 2).
+
+cruzamento([Ind1*_,Ind2*_|Resto],[NInd1,NInd2|Resto1],Index):-
+	gerar_pontos_cruzamento(P1,P2),
+	prob_cruzamento(Pcruz),random(0.0,1.0,Pc),
+	((Pc =< Pcruz,!,
+        cruzar(Ind1,Ind2,P1,P2,NInd1),
+	  cruzar(Ind2,Ind1,P1,P2,NInd2))
+	;
+	(NInd1=Ind1,NInd2=Ind2)),
+	Index2 is Index+1,
+	cruzamento(Resto,Resto1,Index2).
+
+preencheh([],[]).
+
+preencheh([_|R1],[h|R2]):-
+	preencheh(R1,R2).
+
+
+sublista(L1,I1,I2,L):-
+	I1 < I2,!,
+	sublista1(L1,I1,I2,L).
+
+sublista(L1,I1,I2,L):-
+	sublista1(L1,I2,I1,L).
+
+sublista1([X|R1],1,1,[X|H]):-!,
+	preencheh(R1,H).
+
+sublista1([X|R1],1,N2,[X|R2]):-!,
+	N3 is N2 - 1,
+	sublista1(R1,1,N3,R2).
+
+sublista1([_|R1],N1,N2,[h|R2]):-
+	N3 is N1 - 1,
+	N4 is N2 - 1,
+	sublista1(R1,N3,N4,R2).
+
+rotate_right(L,K,L1):-
+	tarefas(N),
+	T is N - K,
+	rr(T,L,L1).
+
+rr(0,L,L):-!.
+
+rr(N,[X|R],R2):-
+	N1 is N - 1,
+	append(R,[X],R1),
+	rr(N1,R1,R2).
+
+
+elimina([],_,[]):-!.
+
+elimina([X|R1],L,[X|R2]):-
+	not(member(X,L)),!,
+	elimina(R1,L,R2).
+
+elimina([_|R1],L,R2):-
+	elimina(R1,L,R2).
+
+insere([],L,_,L):-!.
+insere([X|R],L,N,L2):-
+	tarefas(T),
+	((N>T,!,N1 is N mod T);N1 = N),
+	insere1(X,N1,L,L1),
+	N2 is N + 1,
+	insere(R,L1,N2,L2).
+
+
+insere1(X,1,L,[X|L]):-!.
+insere1(X,N,[Y|L],[Y|L1]):-
+	N1 is N-1,
+	insere1(X,N1,L,L1).
+
+cruzar(Ind1,Ind2,P1,P2,NInd11):-
+	sublista(Ind1,P1,P2,Sub1),
+	tarefas(NumT),
+	R is NumT-P2,
+	rotate_right(Ind2,R,Ind21),
+	elimina(Ind21,Sub1,Sub2),
+	P3 is P2 + 1,
+	insere(Sub2,Sub1,P3,NInd1),
+	eliminah(NInd1,NInd11).
+
+
+eliminah([],[]).
+
+eliminah([h|R1],R2):-!,
+	eliminah(R1,R2).
+
+eliminah([X|R1],[X|R2]):-
+	eliminah(R1,R2).
+
+mutacao([],[],_).
+
+mutacao([Ind1|Rest],[Ind1|Rest1], 1):-
+	!,
+	mutacao(Rest, Rest1, 2).
+
+mutacao([Ind|Rest],[NInd|Rest1], V):-
+	prob_mutacao(Pmut),
+	random(0.0,1.0,Pm),
+	((Pm < Pmut,!,mutacao1(Ind,NInd));NInd = Ind),
+	V2 is V+1,
+	mutacao(Rest,Rest1,V2).
+
+mutacao1(Ind,NInd):-
+	gerar_pontos_cruzamento(P1,P2),
+	mutacao22(Ind,P1,P2,NInd).
+
+mutacao22([G1|Ind],1,P2,[G2|NInd]):-
+	!, P21 is P2-1,
+	mutacao23(G1,P21,Ind,G2,NInd).
+mutacao22([G|Ind],P1,P2,[G|NInd]):-
+	P11 is P1-1, P21 is P2-1,
+	mutacao22(Ind,P11,P21,NInd).
+
+mutacao23(G1,1,[G2|Ind],G2,[G1|Ind]):-!.
+mutacao23(G1,P,[G|Ind],G2,[G|NInd]):-
+	P1 is P-1,
+	mutacao23(G1,P1,Ind,G2,NInd).
 
 %===========================================================================================================
 % Predicate to generate task execution sequence
 
-task_time(Task, Start, End, Time, _):-
+task_time(Task, Start, End, Time, _).
 
 
 
@@ -786,8 +1119,9 @@ process_task(Task,[NextTask|Rest]):-
 %===========================================================================================================
 % Predicate to process task list
 
-process_task_list([],_).
+process_task_list(Lista, Index):- length(Lista, Tam), Index >= Tam, !.
 process_task_list([Task|Rest], Index):-
+	trace,
     process_task(Task, Rest),
     move_para_inicio(Index, [Task|Rest], NewList),
     Index2 is Index + 1,
@@ -805,12 +1139,17 @@ move_para_inicio(X, Lista, NovaLista) :-
 % Predicate to call task list processing
 
 :- http_handler('/taskSequence', get_task_sequence, []).
+get_tasks().
 
 get_task_sequence(Request) :-
+    cors_enable(Request, [methods([get])]),
+
+
+
     http_read_json_dict(Request, JsonIn),
     json_to_prolog(JsonIn, TaskList),
+    get_tasks,
     process_task_list(TaskList, 0),
     gera,
     json_to_prolog(JsonOut, Lista2),
     reply_json_dict(JsonOut).
-
